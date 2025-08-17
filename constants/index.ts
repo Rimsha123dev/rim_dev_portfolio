bd084f9e521dece12527a43d77909d4492cd8892
Rimsha123dev <153190159+Rimsha123dev@users.noreply.github.com>
2025-08-18 03:53:23 +0500

    commit

---
+++ constants/index.ts
@@ -0,0 +1,299 @@
+import html from "@/public/Space Portfolio Assets/html.png"
+import insta from "@/public/Space Portfolio Assets/instagram.svg"
+import lkdn from "@/public/Space Portfolio Assets/lkdn.png"
+import fb from "@/public/Space Portfolio Assets/facebook.svg"
+import js from "@/public/Space Portfolio Assets/js.png"
+import css1 from "@/public/Space Portfolio Assets/css.png"
+import tail from "@/public/Space Portfolio Assets/tailwind.png"
+import react from "@/public/Space Portfolio Assets/react.png"
+import redux from "@/public/Space Portfolio Assets/redux.png"
+import next from "@/public/Space Portfolio Assets/next.png"
+import framer from "@/public/Space Portfolio Assets/framer.png"
+import stripe from "@/public/Space Portfolio Assets/stripe.webp"
+import nodejs from "@/public/Space Portfolio Assets/node-js.png"
+import mongodb from "@/public/Space Portfolio Assets/mongodb.png"
+import ts from "@/public/Space Portfolio Assets/ts.png"
+import pstg from "@/public/Space Portfolio Assets/postger.png"
+import prisma from "@/public/Space Portfolio Assets/prisma.webp"
+import graphql from "@/public/Space Portfolio Assets/graphql.png"
+import figma from "@/public/Space Portfolio Assets/figma.png"
+import docker from "@/public/Space Portfolio Assets/docker.webp"
+import fapi from "@/public/Space Portfolio Assets/docker.webp"
+import python from "@/public/Space Portfolio Assets/python.png"
+// import micro from "@/public/Space Portfolio Assets/micro."
+import ai from "@/public/Space Portfolio Assets/ai.png"
+import kong from "@/public/Space Portfolio Assets/kong.png"
+import az from "@/public/Space Portfolio Assets/azure.png"
+import kafka from "@/public/Space Portfolio Assets/kafka.png"
+import sanity from "@/public/Space Portfolio Assets/sanity.png"
+
+
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: html,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: css1,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: js,
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: tail,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: react,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: redux,
+      width: 80,
+      height: 80,
+    },
+    // {
+    //   skill_name: "React Query",
+    //   Image: "/reactquery.png",
+    //   width: 80,
+    //   height: 80,
+    // },
+    {
+      skill_name: "Type Script",
+      Image: ts,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 14",
+      Image: next,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: framer,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: stripe,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: nodejs,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: mongodb,
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: lkdn,
+    },
+    {
+      name: "Facebook",
+      src: fb,
+    },
+    {
+      name: "Instagram",
+      src: insta,
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: html,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: css1,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: js,
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: tail,
+      width: 80,
+      height: 80,
+    },
+    // {
+    //   skill_name: "Material UI",
+    //   Image: "/mui.png",
+    //   width: 80,
+    //   height: 80,
+    // },
+    {
+      skill_name: "React",
+      Image: react,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: redux,
+      width: 80,
+      height: 80,
+    },
+    // {
+    //   skill_name: "React Query",
+    //   Image: "/reactquery.png",
+    //   width: 80,
+    //   height: 80,
+    // },
+    {
+      skill_name: "Type Script",
+      Image: ts,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 14",
+      Image: next,
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: nodejs,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "python",
+      Image: python,
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: mongodb,
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "Fast api",
+      Image: fapi,
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "Postger SQL",
+      Image: pstg,
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Sanity",
+      Image: sanity,
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Prisma",
+      Image: prisma,
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Graphql",
+      Image: graphql,
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "kong",
+      Image: kong,
+      width: 70,
+      height: 70,
+    },
+    // {
+    //   skill_name: "microservice",
+    //   Image: micro,
+    //   width: 70,
+    //   height: 70,
+    // },
+    {
+      skill_name: "Docker",
+      Image: docker,
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Figma",
+      Image: figma,
+      width: 50,
+      height: 50,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "kafka",
+      Image: kafka,
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Gen AI",
+      Image: ai,
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "azure",
+      Image: az,
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  
+  
+  
+  
