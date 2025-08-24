23c03bb6d632653542a062bc301a62f982c575d5
Rimsha123dev <153190159+Rimsha123dev@users.noreply.github.com>
2025-08-24 07:02:34 +0500

    commit9999

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,4 +1,3 @@
-
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -18,4 +17,3 @@ export { metadata, viewport } from 'next-sanity/studio'
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
-
