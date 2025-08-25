3c19c3878c580228433cb62b1ef2c34b587b8ebd
Rimsha123dev <153190159+Rimsha123dev@users.noreply.github.com>
2025-08-26 02:39:21 +0500

    commit34

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -4,5 +4,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // .env me rakho
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!, // usually "production"
   apiVersion: "2025-01-01", // koi recent date
-  useCdn: true, // faster for public data
+  useCdn: false, // faster for public data
 });
